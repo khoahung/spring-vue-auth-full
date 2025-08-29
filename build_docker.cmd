6b88d84f941319c0f3f96fe314b9496da8124de9
Nguyễn Tất Hùng <khoahung@gmail.com>
2025-08-29 13:14:12 +0700

    update code

--- build_docker.cmd
+++ build_docker.cmd
@@ -6,5 +6,6 @@ cd ..
 docker network create --driver bridge --subnet 172.19.0.0/16 --gateway=172.19.0.1 network_default_test
 docker compose -f auth-service/docker-compose.yml up -d
 docker compose -f user-service/docker-compose.yml up -d
+docker compose -f fe-vue/docker-compose.yml up -d
 
 
