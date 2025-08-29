40a4bf6e080bdffc26b7dd5474ff220d54b86c09
Nguyễn Tất Hùng <khoahung@gmail.com>
2025-08-29 14:04:55 +0700

    update code

--- fe-vue/src/api/http.js
+++ fe-vue/src/api/http.js
@@ -2,8 +2,8 @@
 import axios from 'axios';
 import { jwtDecode } from 'jwt-decode'
 
-const API_BASE = import.meta.env.VITE_API_BASE || 'http://172.19.0.3:8082';
-const AUTH_BASE = import.meta.env.VITE_AUTH_BASE || 'http://172.19.0.2:8081';
+const API_BASE = import.meta.env.VITE_API_BASE || 'http://192.168.1.101:8082';
+const AUTH_BASE = import.meta.env.VITE_AUTH_BASE || 'http://192.168.1.101:8081';
 
 let isRefreshing = false;
 let refreshQueue = [];
