d203da3f249256b6eda2d1738af77f71fea68fe8
Nguyễn Tất Hùng <khoahung@gmail.com>
2025-08-29 12:49:23 +0700

    update build file

--- build_docker.cmd
+++ build_docker.cmd
@@ -4,9 +4,7 @@ cd .. && cd user-service
 mvn clean install
 cd ..
 docker network create --driver bridge --subnet 172.19.0.0/16 --gateway=172.19.0.1 network_default_test
-
 docker compose -f auth-service/docker-compose.yml up -d
-
 docker compose -f user-service/docker-compose.yml up -d
 
 
