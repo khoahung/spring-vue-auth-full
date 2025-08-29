a194a87a32acdb062459d3700a0d993e38792c1a
Nguyễn Tất Hùng <khoahung@gmail.com>
2025-08-29 13:41:51 +0700

    update code

--- fe-vue/src/api/http.js
+++ fe-vue/src/api/http.js
@@ -2,8 +2,8 @@
 import axios from 'axios';
 import { jwtDecode } from 'jwt-decode'
 
-const API_BASE = import.meta.env.VITE_API_BASE || 'http://user-service:8082';
-const AUTH_BASE = import.meta.env.VITE_AUTH_BASE || 'http://auth-service:8081';
+const API_BASE = import.meta.env.VITE_API_BASE || 'http://172.19.0.3:8082';
+const AUTH_BASE = import.meta.env.VITE_AUTH_BASE || 'http://172.19.0.2:8081';
 
 let isRefreshing = false;
 let refreshQueue = [];
--- user-service/src/main/java/com/example/utils/UrlServer.java
+++ user-service/src/main/java/com/example/utils/UrlServer.java
@@ -1,5 +1,5 @@
 package com.example.utils;
 
 public class UrlServer {
-	public static final String url_auth_services="http://auth-service:8081";
+	public static final String url_auth_services="http://172.19.0.2:8081";
 }
