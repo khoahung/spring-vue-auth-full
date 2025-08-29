b2b4392988e81db8378c5e022c34a6ba36513f40
Nguyễn Tất Hùng <khoahung@gmail.com>
2025-08-29 11:58:45 +0700

    update code

--- build_docker.cmd
+++ build_docker.cmd
@@ -1,4 +1,4 @@
-docker network create --driver bridge --subnet 172.18.0.0/16 --gateway=172.18.0.1 network_default_test
+docker network create --driver bridge --subnet 172.19.0.0/16 --gateway=172.19.0.1 network_default_test
 
 docker compose -f auth-service/docker-compose.yml up -d
 
